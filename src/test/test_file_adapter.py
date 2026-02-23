2e76cdc12719385ec7ac240f8e005ea5fd2561db
Samuel Corradi <samuelcorradi@gmail.com>
2026-02-23 20:08:48 -0300

    Add unit tests for template retrieval in test_file_adapter.py and test_template.py

---
+++ src/test/test_file_adapter.py
@@ -0,0 +1,12 @@
+import pytest
+from etomyte.core.cms import CMS
+from etomyte.core.server import App
+from etomyte.adapter.file import FileAdapter
+
+def test_get_template():
+    app = App(port=8000)
+    app.set_config("BASE_PATH", "C:\\Users\\samue\\OneDrive - Nortegra\\workspace\\etomyte\\projectX")
+    cms = CMS(app, adapter=FileAdapter(app))
+    assert cms.get_template("/product/cars/MyCar") == "About cars template"
+    result = cms.get_template("/product/cars")
+    assert result == "Index template"
