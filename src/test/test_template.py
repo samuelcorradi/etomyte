5a2aa3850fb9e2cbd8b564f5892b8e34a7d3dfc9
Samuel Corradi <samuelcorradi@gmail.com>
2026-02-25 18:36:15 -0300

    Refactor test_get_template to use Server class instead of App class

--- src/test/test_template.py
+++ src/test/test_template.py
@@ -1,11 +1,11 @@
 import pytest
 from etomyte.core.cms import CMS
-from git_repo.src.etomyte.core.app import Server
+from git_repo.src.etomyte.core.server import Server
 from etomyte.adapter.file import FileAdapter
 
 def test_get_template():
-    app = Server(port=8000)
-    app.set_config("BASE_PATH", "C:\\Users\\samue\\OneDrive - Nortegra\\workspace\\etomyte\\projectX")
-    cms = CMS(app, adapter=FileAdapter(app))
-    assert cms.get_template("/product/cars/MyCar") == "About cars template"
-    assert cms.get_template("/") == "Index template"
+    server = Server(port=8000)
+    server.set_config("BASE_PATH", "C:\\Users\\samue\\OneDrive - Nortegra\\workspace\\etomyte\\projectX")
+    app = CMS(server, adapter=FileAdapter(server))
+    assert app.get_template("/product/cars/MyCar") == "About cars template"
+    assert app.get_template("/") == "Index template"
