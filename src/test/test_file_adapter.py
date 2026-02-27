787aa81ca9577c895a213682e30624efc799b283
Samuel Corradi <samuelcorradi@gmail.com>
2026-02-27 19:48:42 -0300

    Refactor test_get_template to simplify CMS initialization by removing Server dependency

--- src/test/test_file_adapter.py
+++ src/test/test_file_adapter.py
@@ -4,9 +4,8 @@ from git_repo.src.etomyte.core.server import Server
 from etomyte.adapter.file import FileAdapter
 
 def test_get_template():
-    app = Server(port=8000)
-    app.set_config("BASE_PATH", "C:\\Users\\samue\\OneDrive - Nortegra\\workspace\\etomyte\\projectX")
-    cms = CMS(app, adapter=FileAdapter(app))
+    home = "C:\\Users\\samue\\OneDrive - Nortegra\\workspace\\etomyte\\projectX"
+    cms = CMS(adapter=FileAdapter(home))
     assert cms.get_template("/product/cars/MyCar") == "About cars template"
     result = cms.get_template("/product/cars")
     assert result == "Index template"
