a3f7229fc408464eb0c794021c95612594bc4e67
Samuel Corradi <samuelcorradi@gmail.com>
2026-02-27 19:45:20 -0300

    Refactor app function to simplify initialization by removing unused parameters

--- src/etomyte/__init__.py
+++ src/etomyte/__init__.py
@@ -1,14 +1,10 @@
 import os
 from etomyte.core.app import Etomyte
-from etomyte.adapter.file import FileAdapter
 
-def app(home:str=None
-    , port:int=8000
-    , adapter:FileAdapter=None
-    , default_template:str="index")->Etomyte:
+def app(home:str=None)->Etomyte:
     """
     """
     if not home:
         home = os.getenv("ETOMYTE_HOME", ".")
-    app = Etomyte(home, adapter, default_template, port)
+    app = Etomyte(home)
     return app
